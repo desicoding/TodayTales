31d8df50b3591130381ceb4d87fb30df08b9eca9
rulebreakerdude <rulebreakerdude@gmail.com>
2021-12-26 19:11:15 +0530

    new fun website

--- todaytales/pages/index.js
+++ todaytales/pages/index.js
@@ -13,12 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://today-tales.vercel.app/">TodayTales!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>The World's most fun site.</code>
         </p>
 
         <div className={styles.grid}>
