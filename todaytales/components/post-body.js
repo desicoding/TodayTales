30baa264848ea03b3a6d797932f5df027b5d2bfe
rulebreakerdude <rulebreakerdude@gmail.com>
2021-12-29 22:38:30 +0530

    hacked the way around cms

--- todaytales/components/post-body.js
+++ todaytales/components/post-body.js
@@ -1,8 +1,9 @@
 export default function PostBody({ content }) {
+  //console.log(content?.replaceAll('&#x3C;','<').replaceAll('<code>',''))
   return (
     <div>
       <div
-        dangerouslySetInnerHTML={{ __html: content }}
+        dangerouslySetInnerHTML={{ __html: content?.replaceAll('&#x3C;','<').replaceAll('<code>','') }}
       />
     </div>
   )
