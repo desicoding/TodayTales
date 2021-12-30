9cd4dcbb05a41082535967796ede195ead6d0beb
rulebreakerdude <rulebreakerdude@gmail.com>
2021-12-30 16:12:00 +0530

    for live test

--- todaytales/components/post-body.js
+++ todaytales/components/post-body.js
@@ -2,13 +2,14 @@ import Image from 'next/image'
 
 export default function PostBody({ content }) {
   var embeddedContent=content?.replace(/&#x3C;/g,'<').replace(/<code>/g,'').replace(/<\/code>/g,'').replace(/<\/pre>/g,'').replace(/<pre>/g,'')
-  console.log('embeddedContent',embeddedContent)
+  //console.log('embeddedContent',embeddedContent)
   var ImageResized = embeddedContent?.replace(/<img/g,'<img width="100%"')
-  //console.log('ImageResized',ImageResized)
+  var amazonEmbed = ImageResized?.replace(/&#x26;/g,'&')
+  console.log('ImageResized',amazonEmbed)
   return (
     <div>
       <div
-        dangerouslySetInnerHTML={{ __html: ImageResized}}
+        dangerouslySetInnerHTML={{ __html: amazonEmbed}}
       />
     </div>
   )
