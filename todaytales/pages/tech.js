5aa41dba0330d31aa8805163ec7ab64e987cc0fb
rulebreakerdude <rulebreakerdude@gmail.com>
2022-01-09 21:40:00 +0530

    fb debug total

--- todaytales/pages/tech.js
+++ todaytales/pages/tech.js
@@ -19,9 +19,9 @@ export default function Tech(props) {
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-06NRFHRH9X"></Script>
       <Script type="text/javascript" src="/googletag.js"></Script>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6060997364623360"
-      crossorigin="anonymous"></script>
-      <ins class="adsbygoogle"
-          style="display:block"
+      crossOrigin="anonymous"></script>
+      <ins className="adsbygoogle"
+          style={{display:'block'}}
           data-ad-client="ca-pub-6060997364623360"
           data-ad-slot="9183255294"
           data-ad-format="auto"
